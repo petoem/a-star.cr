7a2c9d9ea1b1bafe8ab98708c0bd1bb259a9a337
Michael Petö <michael@petoe.me>
2018-03-28 15:16:58 +0200

    Fix spec randomly failing

--- src/a-star.cr
+++ src/a-star.cr
@@ -11,7 +11,7 @@ module AStar
     start.f = yield start, goal
 
     until open.empty?
-      current = open.sort!.first
+      current = open.sort! { |a, b| a.f <=> b.f }.first
       return reconstruct_path goal if current == goal
 
       open.delete current
@@ -20,7 +20,7 @@ module AStar
       current.neighbor.each do |neighbor, distance|
         next if closed.includes? neighbor
         open << neighbor unless open.includes? neighbor
-        unless (new_g = current.g + distance) >= neighbor.g
+        if (new_g = current.g + distance) < neighbor.g
           neighbor.parent = current
           neighbor.g = new_g
           neighbor.f = new_g + yield neighbor, goal
@@ -34,7 +34,7 @@ module AStar
     nil
   end
 
-  private def reconstruct_path(node)
+  def reconstruct_path(node)
     path = [] of typeof(node)
     path << node
     while node = node.parent
@@ -44,8 +44,6 @@ module AStar
   end
 
   class Node(T)
-    include Comparable(self)
-
     getter data : T
     property parent : self?
     property g : Number::Primitive = Float64::INFINITY
@@ -57,10 +55,6 @@ module AStar
     def connect(node : self, distance : Number::Primitive)
       @neighbor[node] = distance
       node.neighbor[self] = distance
-    end
-
-    def <=>(other : self)
-      @f <=> other.f
     end
 
     def reset
