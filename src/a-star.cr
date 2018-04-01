a1e746f11a3f73811c25d09a314060f6c29b5bee
Michael Petö <michael@petoe.me>
2018-04-01 11:10:33 +0200

    Add some documentation

--- src/a-star.cr
+++ src/a-star.cr
@@ -1,8 +1,11 @@
 require "./a-star/*"
 
+# A* pathfinding algorithm
 module AStar
   extend self
 
+  # Runs A* search from `start` to `goal` and uses block as heuristic function.
+  # Returns `Array(T)` or `Nil` if no path was found.
   def search(start : T, goal : T, &block) forall T
     open = [] of T
     closed = [] of T
@@ -27,13 +30,13 @@ module AStar
         end
       end
     end
-    # at the end make sure to reset all nodes
-    # in case someone wants to rerun the search
     open.map! &.reset
     closed.map! &.reset
     nil
   end
 
+  # Reconstructs the path based on a `Node` (usually the goal).
+  # Returns path as an `Array` with start being the first element and goal last.
   def reconstruct_path(node)
     path = [] of typeof(node)
     path << node
@@ -43,6 +46,8 @@ module AStar
     path.reverse!
   end
 
+  # Represents one node from a graph.
+  # Every `Node(T)` can store something in its data getter e.g. coordinates.
   class Node(T)
     getter data : T
     property parent : self?
@@ -50,13 +55,17 @@ module AStar
     property f : Number::Primitive = Float64::INFINITY
     getter neighbor = Hash(self, Number::Primitive).new
 
+    # Create a new node and optionally set its data field.
     def initialize(@data : T = nil); end
 
+    # Connect `self` to another `node` with a `distance`.
     def connect(node : self, distance : Number::Primitive)
       @neighbor[node] = distance
       node.neighbor[self] = distance
     end
 
+    # Resets F and G values back to its initial state `Float64::INFINITY`.
+    # Returns `self`.
     def reset
       @g = Float64::INFINITY
       @f = Float64::INFINITY
